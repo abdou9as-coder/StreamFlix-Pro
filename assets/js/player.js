33834054c661705f061788882f680c7b6e14df80
abdou9as-coder <abdou.9as@gmail.com>
2026-06-29 22:17:18 +0100

    Update player.js
    Create server switching logic

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,11 +1,41 @@
-class StreamPlayer{
+const player=document.getElementById("streamflixPlayer");
+const source=document.getElementById("videoSource");
 
-constructor(){
+document.querySelectorAll(".server").forEach(button=>{
 
-console.log("Player Ready");
+button.onclick=()=>{
+
+document.querySelector(".server.active")
+?.classList.remove("active");
+
+button.classList.add("active");
+
+const type=button.dataset.type;
+
+switch(type){
+
+case "mp4":
+
+source.src="";
+
+player.load();
+
+break;
+
+case "m3u8":
+
+console.log("HLS");
+
+break;
+
+case "iframe":
+
+console.log("Iframe");
+
+break;
 
 }
 
-}
+};
 
-new StreamPlayer();
+});
